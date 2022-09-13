edad3089df5569c724ab4ca21942e463df8491dc
程志国 <zhiguo.cheng@amh-group.com>
2022-09-13 22:24:59 +0800

    chore: stash changes

--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,10 @@
 import { View } from '@tarojs/components'
-import React, { Component } from 'react'
-import { AddressPickerProps, AddressPickerState } from '../types'
+import React from 'react'
+import { AddressPickerProps } from '../types'
 import './style/index.scss'
 
-class AddressPicker extends Component<AddressPickerProps, AddressPickerState> {
-  public state: AddressPickerState = {}
-
-  public render(): JSX.Element {
-    return <View className='fta-address-picker'>{this.props.children}</View>
-  }
-}
-
-/**
- * If you prefer React Hooks...
- 
 function AddressPicker(props: AddressPickerProps): JSX.Element {
-  return (
-    <View className='fta-address-picker'>
-      {props.children}
-    </View>
-  )
+  return <View className='fta-address-picker'>{props.children}</View>
 }
-
- */
 
 export default AddressPicker
--- types/index.d.ts
+++ types/index.d.ts
@@ -1,22 +1,8 @@
-import { ComponentClass } from 'react'
-
-export interface AddressPickerProps {}
-
-export interface AddressPickerState {}
-
-declare const AddressPicker: ComponentClass<AddressPickerProps, AddressPickerState>
-
-export default AddressPicker
-
-/**
 // 如果组件使用的是React Hooks 写法， 删除上面的代码，换成以下写法
 import { FC } from 'react'
 
-export interface AddressPickerProps {
-
-}
+export interface AddressPickerProps {}
 
 declare const AddressPicker: FC<AddressPickerProps>
 
 export default AddressPicker
- */
